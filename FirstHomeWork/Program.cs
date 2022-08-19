23d3cfa1a597bde6ab089f8ce78dbec3f555afa4
КрахМастер КрахМастер <mokrushin.sr@gmail.com>
2022-08-19 12:44:34 +0500

    Добавьте файлы проекта.

---
+++ FirstHomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; // Подключенное пространство имен
+
+namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
+{
+	class Program // Объявление класса Program
+	{
+		static void Main(string[] args) // Объявление метода Main
+		{
+			Console.WriteLine("Привет мир и Склиллбокс"); // Действия внутри метода
+			Console.ReadKey(); 
+		}
+	}
+}
