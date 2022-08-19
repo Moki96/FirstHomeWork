0931a18421dff8ec978de93f29bad923fc2a875d
Moki96 <109303687+Moki96@users.noreply.github.com>
2022-08-19 12:50:41 +0500

    Update Program.cs

--- FirstHomeWork/Program.cs
+++ FirstHomeWork/Program.cs
@@ -7,7 +7,13 @@ namespace FirstApplication.ConsoleApp // Объявление пространс
 		static void Main(string[] args) // Объявление метода Main
 		{
 			Console.WriteLine("Привет мир и Склиллбокс"); // Действия внутри метода
-			Console.ReadKey(); 
+			                                               // Задание 1
+			
+			
+			Console.Write("Привет "); // Задание 2
+			Console.Write("Мир");
+			
+			Console.ReadKey();
 		}
 	}
 }
